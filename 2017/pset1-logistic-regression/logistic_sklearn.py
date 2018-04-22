5dfaca318713f49e4964d21fb52f02926dfb6b2a
Ilya Rudyak <ilyarudyak@yahoo.com>
2018-04-22 12:41:33 +0300

    plot data and sklearn decision boundary

---
+++ 2017/pset1-logistic-regression/logistic_sklearn.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+from sklearn.linear_model import LogisticRegression
+from plotting_data import *
+
+
+def logistic_sklearn(X, y):
+    lr = LogisticRegression()
+    lr.fit(X, y)
+    theta = np.append(lr.intercept_, lr.coef_)
+    return theta
+
+
+if __name__ == '__main__':
+    X, y = get_data()
+    theta = logistic_sklearn(X, y)
+
+    plot_data(X, y)
+    plot_decision_boundary(X, y, theta)
+    plt.show()
---
+++ 2017/pset1-logistic-regression/plotting_data.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+
+def get_data():
+    X = pd.read_csv('logistic_x.txt', '\s+', header=None).as_matrix()
+    y = pd.read_csv('logistic_y.txt', header=None).as_matrix().reshape(X.shape[0])
+    return X, y
+
+
+def plot_data(X, y):
+    X_pos, X_neg = X[y == 1, :], X[y == -1, :]
+
+    plt.plot(X_pos[:, 0], X_pos[:, 1], 'r.')
+    plt.plot(X_neg[:, 0], X_neg[:, 1], 'b.')
+
+
+def plot_data_and_show(X, y):
+    plot_data(X, y)
+    plt.show()
+
+
+def plot_decision_boundary(X, y, theta):
+    t0, t1, t2 = theta
+    x1 = np.linspace(np.floor(np.min(X[:, 0])), np.floor(np.max(X[:, 0])), 100)
+    x2 = -(t0 + t1 * x1) / t2
+    plt.plot(x1, x2, 'k')
+
+
+if __name__ == '__main__':
+    X, y = get_data()
+    plot_data_and_show(X, y)
