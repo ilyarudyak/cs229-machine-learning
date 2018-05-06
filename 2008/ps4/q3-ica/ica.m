5142ce7a898895db69f0b0ab3dc30cc9375ed36f
Ilya Rudyak <ilyarudyak@yahoo.com>
2018-05-06 17:14:39 +0300

    finish ica

--- 2008/ps4/q3-ica/ica.m
+++ 2008/ps4/q3-ica/ica.m
@@ -1,16 +1,19 @@
-function W = ica(X, chunk)
+function W = ica(X)
 
 %%% YOUR CODE HERE
-    [n,m] = size(X);
+    [n, m] = size(X);
+    chunk = 100;
     alpha = 0.0005;
     W = eye(n);
 
-    for iter=1:10,
+    for iter=1:10
         disp([num2str(iter)]);
-        X = X(:,randperm(m));
-        for i=1:floor(m/chunk),
-            Xc = X(:,(i-1)*chunk+1:i*chunk);
-            dW = (1 - 2./(1+exp(-W*Xc)))*Xc' + chunk*inv(W');
-            W = W + alpha*dW;
-        end 
+        X = X(:, randperm(m));
+        for i = 1:floor(m/chunk)
+            Xc = X(:, (i-1)*chunk+1:i*chunk);
+            grad = 1 - 2 * sigmoid(W * Xc);
+            grad = grad * Xc';
+            grad += inv(W') * chunk;
+            W += alpha * grad;
+        end
     end
