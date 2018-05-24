386ec11324e9eb43169410ce0460f4fb6b4fa200
Ilya Rudyak <ilyarudyak@yahoo.com>
2018-05-24 18:30:57 +0300

    done data preprocessing

--- udacity/charityML/charityML.py
+++ udacity/charityML/charityML.py
@@ -5,13 +5,17 @@ import matplotlib.pyplot as plt
 
 import visuals as vs
 
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+
 
 def get_data():
     data = pd.read_csv("census.csv")
+    features_raw = data.drop('income', axis=1)
     income = data.income.copy()
     income[data.income == '<=50K'] = 0
     income[data.income == '>50K'] = 1
-    return data, income
+    return data, features_raw, income
 
 
 def data_exploration():
@@ -23,9 +27,32 @@ def data_exploration():
           f'n_at_most_50k={n_at_most_50k:,} greater_percent={greater_percent*100:.1f}%')
 
 
-if __name__ == '__main__':
-    data, income = get_data()
-    # data_exploration()
+def data_preprocess():
+    # Log-transform the skewed features
+    skewed = ['capital-gain', 'capital-loss']
+    features_log_transformed = pd.DataFrame(data=features_raw)
+    features_log_transformed[skewed] = features_raw[skewed].apply(lambda x: np.log(x + 1))
+    # vs.distribution(features_log_transformed, transformed=True)
 
-    vs.distribution(data)
-    plt.show()
+    # Normalize numerical features
+    scaler = MinMaxScaler()  # default=(0, 1)
+    numerical = ['age', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week']
+    features_log_minmax_transform = pd.DataFrame(data=features_log_transformed)
+    features_log_minmax_transform[numerical] = scaler.fit_transform(features_log_transformed[numerical])
+
+    # One-hot encode
+    features_final = pd.get_dummies(features_log_minmax_transform)
+    return features_final
+
+
+if __name__ == '__main__':
+    data, features_raw, income = get_data()
+    features_final = data_preprocess()
+    X_train, X_test, y_train, y_test = train_test_split(features_final,
+                                                        income,
+                                                        test_size=0.2,
+                                                        random_state=0)
+
+    print("Training set has {} samples.".format(X_train.shape[0]))
+    print("Testing set has {} samples.".format(X_test.shape[0]))
+
--- udacity/charityML/notes.md
+++ udacity/charityML/notes.md
@@ -1,16 +1,27 @@
 ### table of contents 
 
-- **Exploring the Data.**
+- **Exploring the Data.** 
+    - The total number of records, `'n_records'`
+    - The number of individuals making more than $50,000 annually, `'n_greater_50k'`.
+    - The number of individuals making at most $50,000 annually, `'n_at_most_50k'`.
+    - The percentage of individuals making more than $50,000 annually, `'greater_percent'`.
+
 - **Preparing the Data.**
+    - Transforming skewed continuous features (`'capital-gain', 'capital-loss'`).
+    - Normalizing numerical features (using `sklearn.preprocessing.MinMaxScaler`).
+    - One-hot encoding of categorical features (using `pandas.get_dummies()`).
+
 - **Evaluating Model Performance.** In this section, we will investigate four 
 different algorithms, and determine which is best at modeling the data. 
 Three of these algorithms will be supervised learners of your choice, 
 and the fourth algorithm is known as a naive predictor.
+
 - **Improving Results.** In this final section, you will choose from 
 the three supervised learning models the best model to use on the student data. 
 You will then perform a grid search optimization for the model over 
 the entire training set (X_train and y_train) by tuning at least one parameter 
 to improve upon the untuned model's F-score.
+
 - **Feature Importance.**
 
 ### goal
