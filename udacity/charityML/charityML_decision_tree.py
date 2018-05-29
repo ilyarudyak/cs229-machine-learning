b2afd4d08d868a9aba4399e9f900d383a9485747
Ilya Rudyak <ilyarudyak@yahoo.com>
2018-05-29 18:04:14 +0300

    add classif. report and conf. matrix

--- udacity/charityML/charityML_decision_tree.py
+++ udacity/charityML/charityML_decision_tree.py
@@ -4,7 +4,7 @@ import seaborn as sns; sns.set()
 
 from sklearn.model_selection import GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import fbeta_score, make_scorer
+from sklearn.metrics import fbeta_score, make_scorer, classification_report, confusion_matrix
 from sklearn.model_selection import learning_curve
 
 from charityML import split_data
@@ -56,14 +56,35 @@ def get_fbeta05_scoring():
     return make_scorer(fbeta_score, beta=.5)
 
 
+def get_report():
+    dtc = DecisionTreeClassifier(max_depth=6)
+    dtc.fit(X_train, y_train)
+    y_pred = dtc.predict(X_test)
+    return classification_report(y_test, y_pred)
+
+
+def plot_confusion_matrix():
+    dtc = DecisionTreeClassifier(max_depth=6)
+    dtc.fit(X_train, y_train)
+    y_pred = dtc.predict(X_test)
+    mat = confusion_matrix(y_test, y_pred)
+    sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False, cmap="Blues")
+    plt.xlabel('true label')
+    plt.ylabel('predicted label')
+    plt.show()
+
+
 if __name__ == '__main__':
-    # X_train, X_test, y_train, y_test = split_data(sample=.1)
-    #
+    X_train, X_test, y_train, y_test = split_data(sample=.1)
+
     # max_depth_params = {'max_depth': np.arange(1, 11)}
     # dtc_best = fit_model(X_train, y_train, max_depth_params)
     # min_samples_split = {'min_samples_split': np.linspace(50, 600, 12).astype(int)}
     # dtc_best = fit_model(X_train, y_train, min_samples_split)
     # print(dtc_best.get_params()['min_samples_split'])
 
-    X_train, X_test, y_train, y_test = split_data(sample=1)
-    plot_learning_curve()
+    # X_train, X_test, y_train, y_test = split_data(sample=1)
+    # plot_learning_curve()
+
+    print(get_report())
+    # plot_confusion_matrix()
